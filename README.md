9dfc657b088141cede06cb687c5ebac5c094e0c2
cmjiwa <jinghuan.wang@tuni.fi>
2022-05-02 22:28:19 +0300

    Now .ts file works

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ study following https://bearit.udemy.com/course/understanding-typescript/learn/l
 2. envoke compiler `tsc`
 3. save typescript as a .ts file
 4. compile and run a .ts file `tsc fileName.ts`
+5. open index.html in a browser and input two numbers
+6. click add and check console
+
+Note: change index.html as following:
+
+<!-- <script src="js-only.js" defer></script> -->
+
+    <script src="using_ts.js" defer></script>
--- using_ts.ts
+++ using_ts.ts
@@ -1,9 +1,16 @@
 var button = document.querySelector("button");
-var input1 = document.getElementById("num1");
-var input2 = document.getElementById("num2");
+var input1 = document.getElementById("num1")! as HTMLInputElement;
+var input2 = document.getElementById("num2")! as HTMLInputElement;
 function add(num1, num2) {
-    return num1 + num2;  
+    return +num1 + +num2;  //+ convert string to number
 }
 button.addEventListener("click", function () {
     console.log(add(input1.value, input2.value));
 });
+
+// ! tell tsc compiler that there will be an input
+// as HTMLInputElement is to let compiler know what element it will be
+
+// summary: typescript can use ! and type cast (here HTMLInputElement)
+// as syntax, .ts file will be compiled into .js file, and .js file can be executed by 
+//browser, but not .ts file. Therefore, a .ts file must be converted .js file. 
