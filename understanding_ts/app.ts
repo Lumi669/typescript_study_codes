760aa506dab3eb6d3162d46e45c919cf3f978ad1
cmjiwa <jinghuan.wang@tuni.fi>
2022-05-05 22:48:37 +0300

    data type: function types and callbacks

--- understanding_ts/app.ts
+++ understanding_ts/app.ts
@@ -14,8 +14,17 @@ function add(n1: number, n2: number) {
 //   console.log("Result " + num);
 // }
 
-function printResult(num: number) {
+function printResult(num: number): void {
   console.log("Result " + num);
 }
 
 printResult(add(5, 12));
+
+//how to write function type
+// a function name is combineValue, it has two parameters, both are number type and it return number.
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add; // ok
+//combineValues = printResult; //error!!!
+
+console.log(combineValues(8, 8));
