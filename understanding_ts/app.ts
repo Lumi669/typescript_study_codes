f7081e0e1c16ea7285a953b9bb5a35032561bf84
cmjiwa <jinghuan.wang@tuni.fi>
2022-05-05 18:48:26 +0300

    Alternative logic compared to the last commit, literal type

--- understanding_ts/app.ts
+++ understanding_ts/app.ts
@@ -4,17 +4,21 @@ function combine(
   resultConversion: string //use this allow caller of function to define how the result should be returned
 ) {
   let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2; //now with 3rd param checked, mutst use + to convert input1 and input2 as number
   } else {
     result = input1.toString() + input2.toString();
   }
+  return result;
 
-  if (resultConversion === "as-number") {
-    return +result;
-  } else {
-    return result.toString();
-  }
+  // if (resultConversion === "as-number") {
+  //   return +result;
+  // } else {
+  //   return result.toString();
+  // }
 }
 
 const combinedAges = combine(30, 20, "as-number");
