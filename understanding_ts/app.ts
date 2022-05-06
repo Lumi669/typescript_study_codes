356b1cfdcdcdc0d6ab9336e51621a25db8f370e8
cmjiwa <jinghuan.wang@tuni.fi>
2022-05-06 07:34:00 +0300

    data type: unkown, improve comments

--- understanding_ts/app.ts
+++ understanding_ts/app.ts
@@ -1,7 +1,7 @@
 /*
 unkown type is strictive than any type. If not know which data type it is, then 
-use unkown, it can still add type check over any, see below codes in image, userName is string, userInput is unkown, even userInput is initialized as a string, still show error in line 19. However, can use if/else (line 21) to check. any data type is removing any type check, should avoid using it. 
-see bellow unkown type example
+use unkown, it can still add type check over any, see below codes in image, userName is string, userInput is unkown, even userInput is initialized as a string, still show error in line 19. However, can use if/else (line 21) to check. any data type is removing any type check, if change unkown to any in below codes, error disapper, should avoid using it. 
+unkown type need further check with if/else. see bellow unkown type example (comment out line 14 first): 
 */
 
 let userInput: unknown;
