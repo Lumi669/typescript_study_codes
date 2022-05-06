932845c27acaa5159d1d64b79f78ab0ff673feec
cmjiwa <jinghuan.wang@tuni.fi>
2022-05-06 15:56:45 +0300

    data type: never

--- understanding_ts/app.ts
+++ understanding_ts/app.ts
@@ -1,21 +1,18 @@
-/*
-unkown type is strictive than any type. If not know which data type it is, then 
-use unkown, it can still add type check over any, see below codes in image, userName is string, userInput is unkown, even userInput is initialized as a string, still show error in line 19. However, can use if/else (line 21) to check. any data type is removing any type check, if change unkown to any in below codes, error disapper, should avoid using it. 
-unkown type need further check with if/else. see bellow unkown type example (comment out line 14 first): 
-*/
-
-let userInput: unknown;
-let userName: string;
-
-//userInput = 5;
-userInput = "max";
-//userInput = 9;
-
-userName = userInput;
-
-if (typeof userInput === "string") {
-  userName = userInput;
-  console.log("yes");
-} else {
-  console.log("no");
+//Note: below function, generateError, is both void and never type
+// it can not return anything becasue it crashes at throw error, so it belongs to also never type.
+//when hover over generateError, it shows void, this is because never is new type,
+//and it hasn't been built into the first version of typescript yet.
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
 }
+
+generateError("An error occurred!", 500);
+
+//function fail inferred as type never by Typescript
+const fail = (message: string) => {
+  throw new Error(message);
+};
+
+//Note: type never can only assigned to a data that can be assigned as never.
+// e.g. const grade: never = 123; will show error by Typescript, becaue a number cannot be never type.
+//const grade: never = 123;
