fbd559c4c6cbd038fdbb1eb43f12d5cfff717f0e
cmjiwa <jinghuan.wang@tuni.fi>
2022-05-07 14:33:10 +0300

    compiler: code quality options, ensure all branches in a function return if one branch had return

--- understanding_ts/src/app.ts
+++ understanding_ts/src/app.ts
@@ -7,3 +7,10 @@ function clickHandler(message: string) {
 
 //here there is not this keyword, so use null instead of this.
 button.addEventListener("click", clickHandler.bind(null, "hello"));
+
+function add(n1: number, n2: number) {
+  if (n1 + n2 > 0) {
+    return n1 + n2;
+  }
+  return;
+}
