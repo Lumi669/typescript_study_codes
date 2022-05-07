104502bf9ba1afe44be4e46909e294cbe8eda841
cmjiwa <jinghuan.wang@tuni.fi>
2022-05-07 12:12:36 +0300

    compiler: strict compilation, a prameter now define type is ok, no error shown, but with parameter not allowed.

--- understanding_ts/dist/analytics.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-console.log("i wan to go ... ");
--- understanding_ts/src/analytics.ts
+++ understanding_ts/src/analytics.ts
@@ -1,1 +1,9 @@
-console.log("i wan to go ... ");
+function sendAnalytics(data) {
+  console.log(data);
+}
+
+let aa; // note: a prameter now define type is ok, no error shown, but with parameter not allowed.
+sendAnalytics("The data");
+aa = 90;
+
+console.log(aa);
