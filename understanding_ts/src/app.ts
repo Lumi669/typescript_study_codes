548269eb441fcb1cbd32cf7838c3df2dbf3c33b1
cmjiwa <jinghuan.wang@tuni.fi>
2022-05-07 13:11:47 +0300

    compiler: strict end in the commit, strictBindCallApply option

--- understanding_ts/src/app.ts
+++ understanding_ts/src/app.ts
@@ -1,11 +1,9 @@
-const userName = "ros";
-
-console.log(userName);
-// long notes about compilation see typescript_info.odt on google drive.
-
 //must use ! at the end of the code here to tell typescript there is such element, otherwise show error
 const button = document.querySelector("button")!;
 
-button.addEventListener("click", () => {
-  console.log("apple are comming ...");
-});
+function clickHandler(message: string) {
+  console.log("Clicked " + message);
+}
+
+//here there is not this keyword, so use null instead of this.
+button.addEventListener("click", clickHandler.bind(null, "hello"));
