8d7616c68ae7f999985cd696e5e5b4e8f4ea3a27
cmjiwa <jinghuan.wang@tuni.fi>
2022-05-07 10:29:29 +0300

    working with source maps, notes see google doc typescript_info

--- understanding_ts/app.ts
+++ understanding_ts/app.ts
@@ -2,3 +2,10 @@ const userName = "ros";
 
 console.log(userName);
 // long notes about compilation see typescript_info.odt on google drive.
+
+//must use ! at the end of the code here to tell typescript there is such element, otherwise show error
+const button = document.querySelector("button")!;
+
+button.addEventListener("click", () => {
+  console.log("apple");
+});
